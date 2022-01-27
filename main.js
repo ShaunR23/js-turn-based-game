3b673de05ba51b71fef327fc568eed9fae6fd133
ShaunR23 <shaunradford23@gmail.com>
2022-01-27 16:44:32 -0500

    added dmg

--- main.js
+++ main.js
@@ -2,9 +2,16 @@
 
 ("use strict");
 
+HEAD
 const exampleButton = document.querySelector(`.example`);
 const exampleButton2 = document.querySelector(`.example2`);
 const exampleButton3 = document.querySelector(`.example3`);
+
+const fighter1 = document.querySelector(`.fighter-1`);
+const fighter2 = document.querySelector(`.fighter-2`);
+const fighter3 = document.querySelector(`.fighter-3`);
+const fight = document.querySelector ('.fight')
+e8ecb4d (added dmg)
 let playerNum = 0;
 let enemyNum = 0;
 let game;
@@ -52,6 +59,25 @@ const Game = class {
                 break;
         }
     }
+
+    playerAttack(){
+        let enemyRemainingHealth = null
+
+        enemyRemainingHealth = this.enemy.hp - this.player.dmg
+
+        console.log(enemyRemainingHealth)
+        return enemyRemainingHealth; 
+    }
+
+    enemyAttack(){
+        let playerRemainingHealth = null
+
+        playerRemainingHealth = this.player.hp - this.enemy.dmg
+        console.log(playerRemainingHealth)
+        return playerRemainingHealth;
+    }
+
+
 }
 
 const Player = class {
@@ -70,21 +96,28 @@ const Enemy = class {
     }
 }  
 
-exampleButton.addEventListener("click", () => {
+fighter1.addEventListener("click", () => {
     playerNum = 1;
     console.log('you clicked a button!')
     game = new Game();
     console.log(game);
 })
 
-exampleButton2.addEventListener("click", () => {
+fighter2.addEventListener("click", () => {
     playerNum = 2;
     new Game();
 })
 
-exampleButton3.addEventListener("click", () => {
+fighter3.addEventListener("click", () => {
     playerNum = 3;
     new Game();
+})
+
+fight.addEventListener('click',() => {
+game.playerAttack();
+    setTimeout(function(){
+        game.enemyAttack()
+    }, 1000)
 })
 
 
